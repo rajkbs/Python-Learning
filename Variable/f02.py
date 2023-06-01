6c7e5839c38792b248ee4ae42c0043b55a3314e8
Raj Kumar Baliyar Singh <35659326+rajkbs@users.noreply.github.com>
2023-06-01 16:52:11 +0530

    Add files via upload

---
+++ Variable/f02.py
@@ -0,0 +1,11 @@
+#Assigning multiple values
+
+a=b=c=10
+x, y, z = 10, 20, "Hello"
+print("a :",a)
+print("b :",b)
+print("c :",c)
+print("x :",x)
+print("y :",y)
+print("z :",z)
+print(type(z))
---
+++ Variable/f03.py
@@ -0,0 +1,24 @@
+#String Literals
+
+name = "Raj Kumar"
+Name = 'Raj Kumar'
+namE = '''Raj Kumar'''
+
+#'''  ''' can be used for multi line string literals
+
+print("with double quote :",name)
+print("with single quote :",namE)
+print("with three quote :",Name)
+
+#Numeric Literals
+
+mobileNo = 90909090909
+salary = 89000.75
+
+print("Mobile No :", mobileNo)
+print("Salary :", salary)
+
+#Special Literals
+
+_name = None
+print(_name)
